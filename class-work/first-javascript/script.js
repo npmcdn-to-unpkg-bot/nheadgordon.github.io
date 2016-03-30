3169c64eba76f9f25946fcbe2c27273903ad696f
nheadgordon <nadine@glossier.com>
2016-03-30 18:31:45 -0400

    3/30/16

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,26 @@
+alert('this is an alert. this is annoying');
+
+document.write('writing to the document, like a boss');
+
+var num1 = 10 * 10;
+
+document.write(num1);
+
+var num2 = 100 / 10;
+
+document.write(num2);
+
+var num3 = 50 - 17;
+
+document.write(num3);
+
+var num4 = 50 + 17;
+
+document.write(num4);
+
+var myName = 'Nadine Head-Gordon'
+
+document.write(myName);
+
+document.getElementById('title').style.color='red';
+document.getElementById('text-chunk').style.backgroundColor:'blue';
