aa8fbbb36cfc726ae14a17d92df406b94e27afce
nheadgordon <nadine@glossier.com>
2016-05-04 19:33:19 -0400

    5/4

---
+++ class-work/responsive-navbar/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	// console.logs the window.width
+	$(window).resize(function () {
+		console.log($(window).width());
+	})
+
+	$('#burger').click(function () {
+		$('#mobile-nav').slideToggle();
+	});
+})
