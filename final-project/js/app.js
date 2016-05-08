eeae2267ae34f8339f07515aec98bfab18f46431
nheadgordon <nadine@glossier.com>
2016-05-08 05:03:19 -0400

    5:03

--- final-project/js/app.js
+++ final-project/js/app.js
@@ -28,54 +28,5 @@ var elementPosition = $('#vertical-align-index').offset();
 
     // console.logs the window.width
 
-
-$("#aries-icon").on("click", function () {
-    window.location = 'aries.html';
-});
-
-$("#taurus-icon").on("click", function () {
-    window.location = 'taurus.html';
-});
-
-$("#gemini-icon").on("click", function () {
-    window.location = 'gemini.html';
-});
-
-$("#cancer-icon").on("click", function () {
-    window.location = 'cancer.html';
-});
-
-$("#leo-icon").on("click", function () {
-    window.location = 'leo.html';
-});
-
-$("#virgo-icon").on("click", function () {
-    window.location = 'virgo.html';
-});
-
-$("#libra-icon").on("click", function () {
-    window.location = 'libra.html';
-});
-
-$("#scorpio-icon").on("click", function () {
-    window.location = 'scorpio.html';
-});
-
-$("#sagittarius-icon").on("click", function () {
-    window.location = 'sagittarius.html';
-});
-
-$("#capricorn-icon").on("click", function () {
-    window.location = 'capricorn.html';
-});
-
-$("#aquarius-icon").on("click", function () {
-    window.location = 'aquarius.html';
-});
-
-$("#pisces-icon").on("click", function () {
-    window.location = 'pisces.html';
-});
-
 })
 
